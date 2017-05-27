373431a8524aac38d312b7a335268d47b49398c1
iwaken71 <iwaken59371518@gmail.com>
2017-05-28 03:06:52 +0900

    消える実装

--- Assets/Scripts/NamePlate.cs
+++ Assets/Scripts/NamePlate.cs
@@ -15,8 +15,15 @@ public class NamePlate : MonoBehaviour {
 	Transform cameraTransform;
 
 	[SerializeField] 
+	Image plane,image;
+	float defaultNameAlpha,defaultNumberAlpha,defaultPlaneAlpha,defaultImageAlpha;
+
+
 
 	public bool IsSmooth = true;
+
+	float timer = 0;
+	float limit = 4;
 
 //d	string name;
 
@@ -25,12 +32,48 @@ public class NamePlate : MonoBehaviour {
 	//	Debug.Log(this.name);
 		distination = Vector3.zero;
 		cameraTransform = Camera.main.transform;
+		defaultNameAlpha = nameLabel.color.a;
+		defaultNumberAlpha = numberLabel.color.a;
+		defaultPlaneAlpha = plane.color.a;
+		defaultImageAlpha = image.color.a;
 	}
 
 	// Use this for initialization
 	void Start () {
 		
 		Time.fixedDeltaTime = 0.05f;
+	}
+
+	public void ResetAlpha(){
+//		nameLabel.color = PlusAlpha (nameLabel.color,defaultNameAlpha);
+//		numberLabel.color = PlusAlpha(numberLabel.color,defaultNumberAlpha);
+//		plane.color = PlusAlpha (plane.color,defaultPlaneAlpha);
+//		image.color = PlusAlpha(image.color,defaultImageAlpha);
+		timer = 0;
+
+	}
+
+	Color PlusAlpha (Color c, float a)
+	{
+		return new Color(c.r,c.g,c.b,a);
+	}
+
+	void Update ()
+	{
+		SetUIColor ();
+		if (timer <= limit) {
+			timer += Time.deltaTime;
+
+		}
+
+	}
+
+	void SetUIColor ()
+	{
+		nameLabel.color = Color.Lerp(PlusAlpha(nameLabel.color,defaultNameAlpha),PlusAlpha(nameLabel.color,0),timer/limit);
+		numberLabel.color = Color.Lerp(PlusAlpha(numberLabel.color,defaultNumberAlpha),PlusAlpha(numberLabel.color,0),timer/limit);
+		plane.color = Color.Lerp(PlusAlpha(plane.color,defaultPlaneAlpha),PlusAlpha(plane.color,0),timer/limit);
+		image.color = Color.Lerp(PlusAlpha(image.color,defaultImageAlpha),PlusAlpha(image.color,0),timer/limit);
 	}
 
 
@@ -50,10 +93,12 @@ public class NamePlate : MonoBehaviour {
 		//this.name = input;
 		nameLabel.text = input;
 		distination = transform.position;
+		ResetAlpha();
 	}
 
 	public void SetDistination(Vector3 input){
 		distination = input;
+		ResetAlpha();
 	}
 
 	public void SetNumber(float number){
